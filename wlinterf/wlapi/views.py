b9c86566b83348ee1737c837eb42d55508bf87bd
James W <james.wu@alumni.olin.edu>
2016-12-13 00:41:25 -0500

    set timezone

--- wlinterf/wlapi/views.py
+++ wlinterf/wlapi/views.py
@@ -4,8 +4,6 @@ from django.views.generic import ListView
 from wlapi.models import Visit
 from django.http import JsonResponse
 from django.views.generic.edit import CreateView
-#from django.views.decorators.csrf import csrf_exempt
-#from django.views.decorators import method_decorator
 
 # Create your views here.
 
